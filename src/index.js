7bcebd3720cb73b7bd6f71ebb3cadd078d359768
Ki Song <kiwisquash@gmail.com>
2019-06-27 09:38:21 -0700

    Added some padding.

--- src/index.js
+++ src/index.js
@@ -7,28 +7,30 @@ const Clip = (props) => {
 }
 
 const Pad = (props) => {
-	return (<div className="drum-pad">
-		{props.innertext}
+	return (<div className={"drum-pad "+ props.row}>
+		<div>{props.innertext}</div>
 		<Clip src={props.src} id={props.innertext}/>	
 	</div>);
 }
 
 const Display = (props) => {
 	return (<div id="display">
-		<Pad src="temp" innertext="Q"/>
-		<Pad src="temp" innertext="W"/>
-		<Pad src="temp" innertext="E"/>
-		<Pad src="temp" innertext="A"/>
-		<Pad src="temp" innertext="S"/>
-		<Pad src="temp" innertext="D"/>
-		<Pad src="temp" innertext="Z"/>
-		<Pad src="temp" innertext="X"/>
-		<Pad src="temp" innertext="C"/>
+		<Pad row="row1" src="temp" innertext="Q"/>
+		<Pad row="row1" src="temp" innertext="W"/>
+		<Pad row="row1" src="temp" innertext="E"/>
+		<Pad row="row2" src="temp" innertext="A"/>
+		<Pad row="row2" src="temp" innertext="S"/>
+		<Pad row="row2" src="temp" innertext="D"/>
+		<Pad row="row3" src="temp" innertext="Z"/>
+		<Pad row="row3" src="temp" innertext="X"/>
+		<Pad row="row3" src="temp" innertext="C"/>
 	</div>);
 }
 
 ReactDOM.render(
 	<div id="drum-machine">
+		<div className="filler"/>
 		<Display/>
+		<div className="filler"/>
 	</div>, document.getElementById('root')
 );
